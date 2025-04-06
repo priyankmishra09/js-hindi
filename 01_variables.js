e3639620e0e510b59637cd6fb0b1febd7650062e
priyankmishra09 <priyankmishra09@gmail.com>
2025-04-06 08:34:29 +0000

    first commit

---
+++ 01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 1234567890;
+let accountName = "John Doe";
+let accountPassword = "password123";
+let accountCity = "New York";
+let accountstate;
+// accountId = 9876543210; // This will throw an error because accountId is a constant
+console.log(accountId);
+console.table([accountId, accountName, accountPassword, accountCity, accountstate]);
